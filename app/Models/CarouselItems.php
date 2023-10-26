96e55ef212a67223a2b6ebaa74f6abd53a01d43e
kathabecia <katherinerose.eman18@gmail.com>
2023-10-26 15:12:54 +0800

    api controller and seed setup

--- app/Models/CarouselItems.php
+++ app/Models/CarouselItems.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class CarouselItems extends Model
 {
     use HasFactory;
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'carousel_items';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'carousel_item_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'carousel_name',
+        'image_path',
+        'description',
+        'user_id',
+    ];
+
 }
